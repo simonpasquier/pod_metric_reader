4afeebeb5ad5113c5411d8ae091bb6d49bd6b53f
Simon Pasquier <spasquie@redhat.com>
2023-11-13 10:46:28 +0100

    Update generate_manifests.sh

--- generate_manifests.sh
+++ generate_manifests.sh
@@ -1,11 +1,92 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+declare SHOW_USAGE=false
+declare NAMESPACES=16
+declare PODS=1
+declare SECRETS=16
+declare SECRET_SIZE=1024
+
+parse_args() {
+	### while there are args parse them
+	while [[ -n "${1-}" ]]; do
+		case $1 in
+			-h | --help)
+			SHOW_USAGE=true
+			# exit the loop
+			break
+			;;
+		--namespaces | -n)
+			shift
+			NAMESPACES=$1
+			shift
+			;;
+		--pods | -p)
+			shift
+			PODS=$1
+			shift
+			;;
+		--secrets | -s)
+			shift
+			SECRETS=$1
+			shift
+			;;
+		--secret-size)
+			shift
+			SECRET_SIZE=$1
+			shift
+			;;
+		*)
+			shift
+			;;
+		esac
+	done
+	return 0
+}
+
+show_usage() {
+	local name
+	name="$(basename "$0")"
+
+	read -r -d '' help <<-EOF_HELP || true
+Usage:
+────────────────────────────────────────────
+  ❯ $name [--namespaces <number>] [--secrets <number>] [--secret-size <number>]
+  ❯ $name  -h|--help
+
+Options:
+⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻
+  --help | -h         Show this help
+  --namespaces | -n: Defines the number of namespaces to generate.
+  --pods | -p:       Defines the number of pods (per namespace) to generate.
+  --secrets | -s:    Defines the number of secrets (per namespace) to generate.
+  --secret-size:     Defines the size (in bytes) of secret data
+
+EOF_HELP
+
+	echo -e "$help"
+	return 0
+}
+
+parse_args "$@" || {
+	show_usage
+	exit 1
+}
+
+generate_random() {
+    tr -dc '[:alpha:]' < /dev/urandom | fold -w "$1" | head -n 1
+}
+
+$SHOW_USAGE && {
+	show_usage
+	exit 0
+}
+
+echo "Generating manifests with $NAMESPACES namespaces, $PODS pods and $SECRETS secrets..."
+
 rm -f ns.yaml pods.yaml secrets.yaml
 
-NB=${1:-1024}
-
-for i in $(seq 1 "$NB"); do
+for i in $(seq 1 "$NAMESPACES"); do
   cat <<EOF >> ns.yaml
 ---
 apiVersion: v1
@@ -16,7 +97,7 @@ metadata:
     kubernetes.io/name: test
 EOF
 
-  for j in $(seq 1 1); do
+  for j in $(seq 1 "$PODS"); do
     cat <<EOF >> pods.yaml
 ---
 apiVersion: v1
@@ -35,7 +116,7 @@ spec:
 EOF
   done
 
-  for j in $(seq 1 4); do
+  for j in $(seq 1 "$SECRETS"); do
     cat <<EOF >> secrets.yaml
 ---
 apiVersion: v1
@@ -44,10 +125,12 @@ metadata:
   name: test-$j
   namespace: test-$i
   annotations:
-    kubernetes.io/description: $(tr -dc '[:alpha:]' < /dev/urandom | fold -w 1024 | head -n 1)
+    kubernetes.io/description: $(generate_random 1024)
   labels:
     kubernetes.io/name: test
-data: {}
+stringData:
+  key: |-
+    $(generate_random "$SECRET_SIZE")
 EOF
   done
 done
